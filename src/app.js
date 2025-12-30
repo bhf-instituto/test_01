ce57597b32df6e9b8a75bc5b3591863d94bc6010
bhf.instituto <bauhoffermariano.instituto@gmail.com>
2025-12-30 02:14:36 -0300

    a

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRouter from './routes/user.route.js'
+import dbConnection from './config/connectionMySQL.js'
 
 const app = express();
 
@@ -9,6 +10,15 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Soy la respuesta 01")
 })
+
+app.get("/health/db", async (req, res) => {
+  try {
+    const [rows] = await dbConnection.query("SELECT 1");
+    res.json({ ok: true });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 app.use('/user', userRouter)
 
